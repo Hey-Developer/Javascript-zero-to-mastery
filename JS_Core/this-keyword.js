2d4806bd4fdfec298e6b2b19f7fcb92a31ec2612
DɪᴠʏᴀɴSH TʜᴀᴋᴜR <hey.devloper@gmail.com>
2020-08-30 15:12:15 +0530

    Bug Fixes

--- JS_Core/this-keyword.js
+++ JS_Core/this-keyword.js
@@ -1,4 +1,4 @@
-/* 
+/*
 $ What the hell is 'this'?
 ?-The JavaScript this keyword refers to the object it belongs to.
 
@@ -20,15 +20,15 @@ let person = {
     full_name: function() {
         return this.first_name + " " + this.last_name;
     },
-    /* 
-                                                    In an object method, this refers to the "owner" of the method.i.e this===person
-                                                    so this.first_name=== person.first_name
-                                                */
+    /*
+                                                        In an object method, this refers to the "owner" of the method.i.e this===person
+                                                        so this.first_name=== person.first_name
+                                                    */
 };
 
 // 'this' alone
 var x = this;
-/* 
+/*
 When used alone, the owner is the Global object, so this refers to the Global object.
 In a browser window the Global object is [object Window]:
 OR-->
@@ -45,7 +45,7 @@ let print = function() {
 var a = 10; // now the global object is change to a hence this will refer to a
 print();
 
-/* 
+/*
 > 'this' in a function in strict mode:
     JavaScript strict mode does not allow default binding.
     So, when used in a function, in strict mode, this is undefined.
@@ -56,10 +56,10 @@ function myFunction() {
  */
 
 //  'this' in Event Handlers:In HTML event handlers, this refers to the HTML element that received the event:
-/* 
+/*
 <button onclick="this.style.display='none'">
   Click to Remove Me!
-</button> 
+</button>
 */
 
 //> Object Method Binding:
@@ -102,7 +102,7 @@ var student = {
 // now if call this method sayName in some another variable???
 var greetStudent = student.sayName(); //undefined
 // Because here we lost its default method binding for this keyword so whats the solution.
-/* 
+/*
 ? way One: student.sayName({name:"test"})
 here function is called and the context of this is change
 ? Way two: bind()
